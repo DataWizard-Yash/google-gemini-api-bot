ccd5abdfaa667f635abd172d978cb3e0c909d126
DataWizard-Yash <yashbhatt.datascience@gmail.com>
2024-02-22 22:42:16 +0530

    added one more chatbot endpoint

--- app/main.py
+++ app/main.py
@@ -1,8 +1,30 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import requests
+import markdown
+import google.generativeai as genai
 
 app = FastAPI()
+
+genai.configure(api_key="AIzaSyAK1vDq6hoOv6pFCLkMIUFYJsVquOLEu8Y")
+
+generation_config = {
+    "temperature": 0.9,
+    "top_p": 1,
+    "top_k": 1,
+    "max_output_tokens": 2048,
+}
+
+safety_settings = [
+    {"category": "HARM_CATEGORY_HARASSMENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+    {"category": "HARM_CATEGORY_HATE_SPEECH", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+    {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+    {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+]
+
+model = genai.GenerativeModel(
+    model_name="gemini-1.0-pro", generation_config=generation_config, safety_settings=safety_settings
+)
 
 class InputText(BaseModel):
     text: str
@@ -12,6 +34,14 @@ class OutputText(BaseModel):
 
 @app.post("/bot", response_model=OutputText)
 async def generate_story(payload: InputText):
+    try:
+        response = model.continue_chat(payload.text)
+        return {"generated_text": response}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@app.post("/chat/", response_model=OutputText)
+async def chat_with_model(payload: InputText):
     url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=AIzaSyAK1vDq6hoOv6pFCLkMIUFYJsVquOLEu8Y"
     headers = {'Content-Type': 'application/json'}
     try:
@@ -19,6 +49,7 @@ async def generate_story(payload: InputText):
         response.raise_for_status()
         # Extract the generated text content from the response JSON
         generated_text = response.json()['candidates'][0]['content']['parts'][0]['text']
-        return {"generated_text": generated_text}
+        formatted_response = markdown.markdown(generated_text)
+        return {"generated_text": formatted_response}
     except requests.exceptions.RequestException as e:
         raise HTTPException(status_code=500, detail=str(e))
